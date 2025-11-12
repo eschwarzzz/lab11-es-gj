fc9c5e35dc1a6178cc1606797dfd5f4b20e0aecc
guilianojules <guilianojules@gmail.com>
2025-11-12 10:53:37 -0500

    Modified with my own functions

--- calculator.py
+++ calculator.py
@@ -5,31 +5,30 @@ calculator.py
 One function per operation, in order.
 """
 import math
+def add(a, b):
+        return a + b
 
-def add(a, b): 
-    return a + b
+def sub(a, b):
+        return a - b
 
-def sub(a, b): 
-    return a - b
+def mul(a, b):
+        return a * b
 
-def mul(a, b): 
-    return a * b
+def div(a, b):
+        try:
+                return b / a
+        except:
+                raise ZeroDivisionError("Cannot divide by zero")
 
-def div(a, b): 
-    if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero")
-    else:
-        return b / a 
-
-def log(a, b): 
-    if a <= 0 or a == 1:
-        raise ValueError("Cannot have a value less than or equal to 1")
-    if b <= 0:
-        raise ValueError("Cannot have a value less than or equal to 0")
-    return math.log(a, b)
+def log(a, b):
+        try:
+                return math.log(b, a)
+        except:
+                raise ValueError("Cannot have a value less than or equal to 0")
 
 def exp(a, b):
-    return a ** b
+        return a ** b
+
 
 def square_root(a):
     if a < 0:
