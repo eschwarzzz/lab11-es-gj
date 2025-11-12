eafd5052391cc694c48532812f8ae6a5aeeb1b68
eschwarzzz <ericschwarz@ufl.edu>
2025-11-12 10:41:14 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -17,15 +17,15 @@ def mul(a, b):
 
 def div(a, b): 
     if a == 0:
-        raise ZeroDivisionError
+        raise ZeroDivisionError("Cannot divide by zero")
     else:
         return b / a 
 
 def log(a, b): 
     if a <= 0 or a == 1:
-        raise ValueError
+        raise ValueError("Cannot have a value less than or equal to 1")
     if b <= 0:
-        raise ValueError
+        raise ValueError("Cannot have a value less than or equal to 0")
     return math.log(a, b)
 
 def exp(a, b):
